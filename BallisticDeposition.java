5f7a1fcd7e3d7b742aa5c7b3c94d75d561f81a23
tylerparsons <tylereparsons13@gmail.com>
2015-01-28 17:22:29 -0500

    Synced Large-Systems with master

---
+++ BallisticDeposition.java
@@ -0,0 +1,38 @@
+/*
+######################################
+BallisticDeposition.java
+@author		Tyler Parsons
+@created	7 May 2014
+ 
+A re-implementation of a well known
+surface deposition model based on
+the bonding of atoms. 
+######################################
+*/
+package bdm;
+
+public class BallisticDeposition extends Deposition {
+
+	/*
+	 * Selects a column at random and deposits
+	 * an atom there, letting it fall and bond
+	 * with the the first horizontally or ver-
+	 * tically adjacent neighbor.
+	 * 
+	 * */
+	public void step() {
+		super.step();
+		
+		int col = (int)(Math.random()*L);
+		int h = maxHeight(col - 1, col + 1);
+		if (lattice[col][h] == 1 && isValid(col, h+1)) {
+			lattice[col][h+1] = 1;
+			height[col] = h + 1;
+		}
+		else {
+			lattice[col][h] = 1;
+			height[col] = h;
+		}
+	}
+
+}
